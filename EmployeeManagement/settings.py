b8f12283544f0b6f64f2478e7c002760a58441d5
Asif Muhammad Yousuf <amyousuf29@gmail.com>
2019-01-03 13:15:23 -0700

    Update settings.py

--- EmployeeManagement/settings.py
+++ EmployeeManagement/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*zbn1@$9&$r!+c^%+0^b_1d=0yu#jeop5unq=-g_xz-pd^xy&'
+SECRET_KEY = 'Do not share this'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
